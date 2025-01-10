9d980d502394ca759716f11dfa450c71319fcf5a
Justus Siegert <jubesie@googlemail.com>
2025-01-10 01:12:25 +0100

    Login and register view done

--- sensora-ui/src/views/LoginView.vue
+++ sensora-ui/src/views/LoginView.vue
@@ -10,38 +10,40 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <Card class="mx-auto max-w-sm">
-    <CardHeader>
-      <CardTitle class="text-2xl"> {{ t('login.SignIn') }}</CardTitle>
-      <CardDescription> {{ t('login.SignInInfo') }}</CardDescription>
-    </CardHeader>
-    <CardContent>
-      <div class="grid gap-4">
-        <div class="grid gap-2">
-          <Label for="email">{{ t('login.Email') }}</Label>
-          <Input id="email" placeholder="JoeDoe@sensora.de" required type="email" />
-        </div>
-        <div class="grid gap-2">
-          <div class="flex items-center">
-            <Label for="password">{{ t('login.Password') }}</Label>
-            <a class="ml-auto inline-block text-sm underline" href="#">
-              {{ t('login.ForgotPassword') }}
-            </a>
+  <div class="h-screen flex items-center justify-center bg-gray-100">
+    <Card class="mx-auto max-w-sm w-full">
+      <CardHeader>
+        <CardTitle class="text-2xl"> {{ t('login.SignIn') }}</CardTitle>
+        <CardDescription> {{ t('login.SignInInfo') }}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div class="grid gap-4">
+          <div class="grid gap-2">
+            <Label for="email">{{ t('login.Email') }}</Label>
+            <Input id="email" placeholder="JoeDoe@sensora.de" required type="email" />
           </div>
-          <Input id="password" required type="password" />
+          <div class="grid gap-2">
+            <div class="flex items-center">
+              <Label for="password">{{ t('login.Password') }}</Label>
+              <a class="ml-auto inline-block text-sm underline" href="#">
+                {{ t('login.ForgotPassword') }}
+              </a>
+            </div>
+            <Input id="password" required type="password" />
+          </div>
+          <Button class="w-full" type="submit">{{ t('login.SignIn') }}</Button>
+
+          <Separator :label="t('login.Or')" class="my-4" />
+
+          <Button class="w-full" variant="outline">{{ t('login.SignInWithA') }}</Button>
         </div>
-        <Button class="w-full" type="submit">{{ t('login.SignIn') }}</Button>
-
-        <Separator :label="t('login.Or')" class="my-4" />
-
-        <Button class="w-full" variant="outline">{{ t('login.SignInWithA') }}</Button>
-      </div>
-      <div class="mt-4 text-center text-sm">
-        {{ t('login.DontHaveAccount') }}
-        <router-link :to="{ name: 'register' }" class="underline">
-          {{ t('login.SignUp') }}
-        </router-link>
-      </div>
-    </CardContent>
-  </Card>
+        <div class="mt-4 text-center text-sm">
+          {{ t('login.DontHaveAccount') }}
+          <router-link :to="{ name: 'register' }" class="underline">
+            {{ t('login.SignUp') }}
+          </router-link>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
 </template>
--- sensora-ui/src/views/RegisterView.vue
+++ sensora-ui/src/views/RegisterView.vue
@@ -10,43 +10,45 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <Card class="mx-auto max-w-sm">
-    <CardHeader>
-      <CardTitle class="text-xl">{{ t('login.SignUp') }}</CardTitle>
-      <CardDescription>{{ t('login.SignUpInfo') }}</CardDescription>
-    </CardHeader>
-    <CardContent>
-      <div class="grid gap-4">
-        <div class="grid grid-cols-2 gap-4">
-          <div class="grid gap-2">
-            <Label for="first-name">{{ t('login.FirstName') }}</Label>
-            <Input id="first-name" placeholder="Max" required />
+  <div class="h-screen flex items-center justify-center bg-gray-100">
+    <Card class="mx-auto max-w-sm w-full">
+      <CardHeader>
+        <CardTitle class="text-xl">{{ t('login.SignUp') }}</CardTitle>
+        <CardDescription>{{ t('login.SignUpInfo') }}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div class="grid gap-4">
+          <div class="grid grid-cols-2 gap-4">
+            <div class="grid gap-2">
+              <Label for="first-name">{{ t('login.FirstName') }}</Label>
+              <Input id="first-name" placeholder="Max" required />
+            </div>
+            <div class="grid gap-2">
+              <Label for="last-name">{{ t('login.LastName') }}</Label>
+              <Input id="last-name" placeholder="Robinson" required />
+            </div>
           </div>
           <div class="grid gap-2">
-            <Label for="last-name">{{ t('login.LastName') }}</Label>
-            <Input id="last-name" placeholder="Robinson" required />
+            <Label for="email">{{ t('login.Email') }}</Label>
+            <Input id="email" placeholder="JohnDoe@sensora.de" required type="email" />
           </div>
-        </div>
-        <div class="grid gap-2">
-          <Label for="email">{{ t('login.Email') }}</Label>
-          <Input id="email" placeholder="JohnDoe@sensora.de" required type="email" />
-        </div>
-        <div class="grid gap-2">
-          <Label for="password">{{ t('login.Password') }}</Label>
-          <Input id="password" type="password" />
-        </div>
-        <Button class="w-full" type="submit"> {{ t('login.CreateAccount') }}</Button>
+          <div class="grid gap-2">
+            <Label for="password">{{ t('login.Password') }}</Label>
+            <Input id="password" type="password" />
+          </div>
+          <Button class="w-full" type="submit"> {{ t('login.CreateAccount') }}</Button>
 
-        <Separator :label="t('login.Or')" class="my-4" />
+          <Separator :label="t('login.Or')" class="my-4" />
 
-        <Button class="w-full" variant="outline"> {{ t('login.SignUpWithA') }}</Button>
-      </div>
-      <div class="mt-4 text-center text-sm">
-        {{ t('login.AlreadyHaveAccount') }}
-        <router-link :to="{ name: 'login' }" class="underline">
-          {{ t('login.SignIn') }}
-        </router-link>
-      </div>
-    </CardContent>
-  </Card>
+          <Button class="w-full" variant="outline"> {{ t('login.SignUpWithA') }}</Button>
+        </div>
+        <div class="mt-4 text-center text-sm">
+          {{ t('login.AlreadyHaveAccount') }}
+          <router-link :to="{ name: 'login' }" class="underline">
+            {{ t('login.SignIn') }}
+          </router-link>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
 </template>
