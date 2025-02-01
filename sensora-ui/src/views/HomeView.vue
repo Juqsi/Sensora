6c5f024a17849086962772cb2e98f509791f2d54
Justus Benedict Siegert <Justus.Benedict.S@gmx.de>
2025-02-01 02:24:00 +0100

    start with scroll list

--- sensora-ui/src/views/HomeView.vue
+++ sensora-ui/src/views/HomeView.vue
@@ -3,6 +3,24 @@ import HomeCard from '@/components/HomeCard.vue'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { CirclePlus } from 'lucide-vue-next'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+interface Tip {
+  id: string
+  title: string
+  description: string
+}
+
+const tips: Tip[] = [
+  {
+    id: '1',
+    title: 'Tip 1',
+    description: 'This is the first tipThis is the first tippThis is the first tip',
+  },
+  { id: '2', title: 'Tip 2', description: 'Another useful tip for you' },
+  { id: '3', title: 'Tip 3', description: 'Don’t forget this one!' },
+]
 </script>
 
 <template>
@@ -19,6 +37,26 @@ import { CirclePlus } from 'lucide-vue-next'
       <AvatarFallback>JS</AvatarFallback>
     </Avatar>
   </div>
+
+  <div class="w-full my-2">
+    <h2 class="text-lg my-2">Tipps</h2>
+    <ScrollArea class="w-screen ml-[calc(-50vw+50%)] xl:w-full xl:ml-0">
+      <div class="flex p-4 space-x-4 w-max">
+        <div v-for="tip in tips" :key="tip.id" class="w-[80vw] shrink-1 xl:max-w-4xl">
+          <Card class="w-full border-none bg-secondary">
+            <CardHeader>
+              <CardTitle>{{ tip.title }}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>{{ tip.description }}</p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
+  </div>
+
   <div class="w-full mt-2">
     <div class="flex justify-between items-center">
       <h3 class="text-lg my-2">Zimmer 1</h3>
