a2bfe0a90589d5bd8017b1bae5a0122e697a3a45
Justus Siegert <jubesie@googlemail.com>
2025-02-28 18:53:58 +0100

    add save function plant settings, not finished

--- frontend/src/views/SinglePlantSettingsView.vue
+++ frontend/src/views/SinglePlantSettingsView.vue
@@ -9,39 +9,64 @@ import RoomSelection from '@/components/RoomSelection.vue'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import NavCard from '@/components/NavCard.vue'
-import {useDeviceStore} from "@/stores";
+import {useDeviceStore, usePlantStore} from "@/stores";
 import {Select, SelectTrigger, SelectValue, SelectItem, SelectContent, SelectGroup, SelectLabel} from '@/components/ui/select'
 import {plantAvatars} from '@/components/plant3d/plantAvatars'
-import type {Room} from "@/api";
+import {type Controller, type createPlantBody, type Room} from "@/api"
+import {toast} from "vue-sonner";
 const router = useRouter()
 const { t } = useI18n()
 
 const deviceStore = useDeviceStore()
+const plantStore = usePlantStore()
 
-const selectedEntity = ref<Room | null>(null)
+const name = ref<string>("")
+const selectedRoom = ref<Room | null>(null)
+const selectedSensor = ref<Controller | null>(null)
+const plantType = ref<string>("")
+const selectedAvatar = ref< {label:string, value:string} | null>(null)
+const note = ref<string>("")
+
+const createPlant = ()=>{
+  if (!selectedRoom.value) {
+    toast.warning(t("Please enter a valid room"))
+  }
+  const newPlant : createPlantBody = {
+    name: name.value,
+    room : selectedRoom.value!.rid,
+  }
+  try {
+    plantStore.createPlant(newPlant)
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <template>
   <NavCard :sub-title="t('plant.settings.SubTitle')" :title="t('plant.settings.Title')">
     <CardContent class="grid gap-6">
+
       <div class="grid gap-2">
         <Label for="subject">{{ t('plant.settings.NameOfPlant') }}</Label>
-        <Input id="subject" :placeholder="t('plant.settings.NamePlaceholder')" />
+        <Input v-model="name" id="subject" :placeholder="t('plant.settings.NamePlaceholder')" />
       </div>
+
       <div class="grid gap-4">
         <div class="grid gap-2">
           <Label for="room">{{ t('plant.settings.Room') }}</Label>
-          <RoomSelection id="room" v-model="selectedEntity" />
+          <RoomSelection id="room" v-model="selectedRoom" />
         </div>
         <div class="grid gap-2">
           <Label for="sensor">{{ t('plant.settings.SelectSensor') }}</Label>
-          <Select id="sensor">
+          <Select v-model="selectedSensor" id="sensor">
             <SelectTrigger>
-              <SelectValue placeholder="Select a sensor" />
+              <SelectValue :placeholder="t('plant.settings.SelectSensorPlaceholder')" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
-                <SelectLabel>Sensors</SelectLabel>
+                <SelectLabel>{{t('plant.settings.sensors')}}</SelectLabel>
                 <SelectItem v-for="device in deviceStore.devices" :value="device.did">
                   {{device.did}}
                 </SelectItem>
@@ -53,18 +78,18 @@ const selectedEntity = ref<Room | null>(null)
 
       <div class="grid gap-2">
         <Label for="plantType">{{ t('plant.settings.PlantType') }}</Label>
-        <Input id="plantType" :placeholder="t('plant.settings.PlantTypePlaceholder')" />
+        <Input id="plantType" v-model:value="plantType" :placeholder="t('plant.settings.PlantTypePlaceholder')" />
       </div>
 
       <div class="grid gap-2">
         <Label for="avatar">{{ t('plant.settings.Avatar') }}</Label>
         <Select id="avatar">
           <SelectTrigger>
-            <SelectValue placeholder="Select a fruit" />
+            <SelectValue v-model:value="selectedAvatar" :placeholder="t('plant.settings.AvatarPlaceholder')" />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
-              <SelectLabel>Fruits</SelectLabel>
+              <SelectLabel>{{t('plant.settings.avatar')}}</SelectLabel>
               <SelectItem v-for="avatar in plantAvatars" :value="avatar.value">
                 {{avatar.label}}
               </SelectItem>
@@ -75,12 +100,12 @@ const selectedEntity = ref<Room | null>(null)
 
       <div class="grid gap-2">
         <Label for="description">{{ t('plant.settings.Description') }}</Label>
-        <Textarea id="description" :placeholder="t('plant.settings.DescriptionPlaceholder')" />
+        <Textarea v-model:value="note" id="description" :placeholder="t('plant.settings.DescriptionPlaceholder')" />
       </div>
     </CardContent>
     <CardFooter class="justify-between space-x-2">
       <Button variant="ghost" @click="router.back()">{{ t('plant.settings.Cancel') }}</Button>
-      <Button>{{ t('plant.settings.Submit') }}</Button>
+      <Button @click="createPlant">{{ t('plant.settings.Submit') }}</Button>
     </CardFooter>
   </NavCard>
 </template>
